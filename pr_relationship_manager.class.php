2c4e1106bf6d23ca349e0510aef43a2d2e02fc6b
Seamus Leahy <seamus@seamusleahy.com>
2013-02-08 09:38:15 -0600

    Fleshing out add/remove/update methods

--- pr_relationship_manager.class.php
+++ pr_relationship_manager.class.php
@@ -3,9 +3,12 @@
 class PR_RelationshipManager {
 
 	protected function get_relationship( $name ) {
-
+		return PR_Configuration::get_instances( $name );
 	}
 
+	/**
+	 * Get a post ID
+	 */
 	protected function get_post_id( $post ) {
 		if( !empty( $post->ID ) ) {
 			return $post->ID;
@@ -14,6 +17,9 @@ class PR_RelationshipManager {
 		}
 	}
 
+	/**
+	 * Get an array of post IDs
+	 */
 	protected function get_post_ids( $posts ) {
 		$ret_posts = array();
 
@@ -25,11 +31,17 @@ class PR_RelationshipManager {
 	}
 
 
+	/**
+	 * Get a post object
+	 */
 	protected function get_post_object( $post ) {
 		return get_post( $post );
 	}
 
 
+	/**
+	 * Get an array of post objects
+	 */
 	protected function get_post_objects( $posts ) {
 		// TODO: optimize with a single DB call to pull posts instead of doing it for each one
 		// is_a( $post, 'WP_Post' )
@@ -42,6 +54,32 @@ class PR_RelationshipManager {
 	}
 
 	/**
+	 *	Validate if a relationship object is valid
+	 */
+	protected function validate_relationship( $relationship ) {
+		if( empty( $relationship ) ) {
+			return new WP_Error( 'invalid relationship', __('No relationship found by that name.'), array( $name, $from, $to ) );
+		}
+		return true;
+	}
+
+	/**
+	 *	Validate if a relationship is possible from $from to $to.
+	 */
+	protected function validate_relationship_connections( $relationship, $from, $to ) {
+		if( !in_array( $from->post_type, $relationship->from ) ) {
+			return new WP_Error( 'invalid from post-type', __('The from post-type cannot be used for the relationship') );
+		}
+
+		foreach( $to as $t ) {
+			if( !in_array( $t->post_type, $relationship->to ) ) {
+				return new WP_Error( 'invalid from post-type', __('The to post-type cannot be used for the relationship') );
+			}
+		}
+		return true;
+	}
+
+	/**
 	 * Add relationships to a post.
 	 *
 	 * @param $name string - the name of the relationship field name
@@ -49,7 +87,31 @@ class PR_RelationshipManager {
 	 * @param $to int|post|array - the post IDs or post object to connect
 	 */
 	function add_relationships( $name, $from, $to ) {
+		$from = $this->get_post_object( $from );
+		$to = $this->get_post_objects( $to );
 
+		$relationship = $this->get_relationship( $name );
+
+		// Validate connections
+		$error = $this->validate_relationship( $relationship );
+		if( $error !== true ) {
+			return $error;
+		}
+
+		$error = $this->validate_relationship_connections( $relationship, $from, $to );
+		if( $error !== true ) {
+			return $error;
+		}
+
+		
+		// This will not re-add posts that are already added
+		$existing_connections = get_post_meta( $from->ID, $relationship->name, false );
+
+		foreach ( $to as $t ) {
+			if( !in_array( $t->ID, $existing_connections ) ) {
+				add_post_meta( $from->ID, $relationship->name, $t->ID, false );
+			}
+		}
 	}
 
 
@@ -61,7 +123,20 @@ class PR_RelationshipManager {
 	 * @param $to int|post|array - the post IDs or post object to connect
 	 */
 	function remove_relationships( $name, $from, $to ) {
+		$from = $this->get_post_object( $from );
+		$to = $this->get_post_objects( $to );
 
+		$relationship = $this->get_relationship( $name );
+
+		// Validate connections
+		$error = $this->validate_relationship( $relationship );
+		if( $error !== true ) {
+			return $error;
+		}
+
+		foreach ( $to as $t ) {
+			delete_post_meta( $from->ID, $relationship->name, $t->ID, false );
+		}
 	}
 
 	/**
@@ -73,7 +148,29 @@ class PR_RelationshipManager {
 	 * @param $to int|post|array - the post IDs or post object to connect
 	 */
 	function update_relationships( $name, $from, $to ) {
+		$from = $this->get_post_object( $from );
+		$to = $this->get_post_objects( $to );
 
+		$relationship = $this->get_relationship( $name );
+
+		// Validate connections
+		$error = $this->validate_relationship( $relationship );
+		if( $error !== true ) {
+			return $error;
+		}
+
+		$error = $this->validate_relationship_connections( $relationship, $from, $to );
+		if( $error !== true ) {
+			return $error;
+		}
+
+		// Delete all
+		delete_post_meta( $from->ID, $relationship->name );
+
+		// Add all
+		foreach ( $to as $t ) {
+			add_post_meta( $from->ID, $relationship->name, $t->ID, false );
+		}
 	}
 
 
